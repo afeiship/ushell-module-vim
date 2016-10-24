996859e0326188aba3a5b6ba667e1430fdeae64c
feizheng <1290657123@qq.com>
2016-10-24 23:19:50 +0800

    format

--- initialize.sh
+++ initialize.sh
@@ -5,6 +5,8 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 rm -rf ~/.vim ~/.vimrc
 ln -s ~/git-oschina/backup-centos-files/.vim ~/.vim
 ln -s ~/git-oschina/backup-centos-files/.vim/.vimrc ~/.vimrc
+
+
 cd ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +BundleInstall +qall
