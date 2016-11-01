2a79428111bf5c85bf91f9b5b4e800dc8eaf05d8
feizheng <1290657123@qq.com>
2016-11-01 16:07:29 +0800

    add vim support

--- initialize.sh
+++ initialize.sh
@@ -10,3 +10,8 @@ ln -s ~/git-oschina/backup-centos-files/.vim/.vimrc ~/.vimrc
 cd ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +BundleInstall +qall
+
+
+## macos:
+brew install vim
+# set clipboard=unnamed
