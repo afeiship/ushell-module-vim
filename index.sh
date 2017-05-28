5a48d56d98fceef6e062a67e3631c4319d13ba9e
afeiship <1290657123@qq.com>
2017-05-28 13:33:38 +0800

    update

--- index.sh
+++ index.sh
@@ -5,3 +5,4 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 alias e-vimrc='vim ~/.vimrc';
 alias vim-install='vim +BundleInstall +qall';
 alias vim-init='git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim';
+alias vim-install-spf13='curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh';
